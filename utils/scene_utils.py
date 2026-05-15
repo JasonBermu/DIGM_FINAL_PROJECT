4bcec183e14aa72bc3157963346d87be281f9810
Jab873 <Jab873@Drexel.edu>
2026-05-14 23:09:28 -0400

    Update scene_utils.py

--- utils/scene_utils.py
+++ utils/scene_utils.py
@@ -6,7 +6,7 @@ def create_ground(size=20):
     return ground
 
 def create_tree(tree_type='round', height=2.0):
-trunk = cmds.polyCylinder(r=0.2, h=height, name="trunk")[0]
+    trunk = cmds.polyCylinder(r=0.2, h=height, name="trunk")[0]
     cmds.move(0, height/2, 0, trunk)
     
     if tree_type == 'round':
@@ -15,7 +15,7 @@ trunk = cmds.polyCylinder(r=0.2, h=height, name="trunk")[0]
         canopy = cmds.polyCone(r=0.8, h=2.0, name="canopy")[0]
         
     cmds.move(0, height, 0, canopy)
-    tree_grp = cmds.group(trunk, foliage, n=f"tree_{tree_type}")
+    tree_grp = cmds.group(trunk, canopy, n=f"tree_{tree_type}_grp")
     return tree_grp
 
 def create_rock(scale_val=1.0):
@@ -23,7 +23,11 @@ def create_rock(scale_val=1.0):
     cmds.setAttr(f"{rock}.scale", scale_val, random.uniform(0.5, 1.5), scale_val)
     return rock
 
-def create_cabin(position = ((0,0,0), scale=1.0)
+def create_cabin(pos=(0, 0, 0), scale=1.0):
+    cabin = cmds.polyCube(n="cabin", w=3, h=2, d=4)[0]
+    cmds.move(pos[0], 1, pos[2], cabin)
+    cmds.scale(scale, scale, scale, cabin)
+    return cabin
 
 def scatter_item(item_name, area_range=10):
   pos_x = random.uniform(-area_range, area_range)
